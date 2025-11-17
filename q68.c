0d8508327c9195aed81856d27b04bfb9da6a7cf0
Navyasinghal1234 <write24navya@gmail.com>
2025-11-18 00:59:49 +0530

    q68.c

---
+++ q68.c
@@ -0,0 +1,29 @@
+//WAP TO DELETE AN ELEMENT FROM AN ARRAY.
+#include <stdio.h>
+
+int main() {
+    int n, i, pos;
+
+    scanf("%d", &n);
+    int arr[n];
+
+    // Read array elements
+    for(i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+
+    scanf("%d", &pos);
+
+    
+    for(i = pos; i < n - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+
+    
+    for(i = 0; i < n - 1; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
